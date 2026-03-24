082da26e6c8881a00737e7269f6a0019e09a9a5a
Francesc <francesc@fxdigital.uk>
2026-03-24 16:11:53 +0100

    fix: added LanguageProvider

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,14 @@
 import './App.css'
 import LanguageSelector from './components/LanguageSelector'
 import Hero from './components/Hero'
+import { LanguageProvider } from './context/LanguageContext'
 
 function App() {
   return (
-    <>
+    <LanguageProvider>
       <LanguageSelector />
       <Hero />
-    </>
+   </LanguageProvider> 
   )
 }
 
