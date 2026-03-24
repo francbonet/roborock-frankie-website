311ee462eb11f079b3d93df765717dcafa5b5eb3
francbonet <francbonet@users.noreply.github.com>
2026-03-24 14:07:07 +0100

    fix: corregir import del hook useLanguage a Hero

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { useLanguage } from '../hooks/useLanguage';
+import { useLanguage } from '../contexts/LanguageContext';
 import './Hero.css';
 
 const Hero: React.FC = () => {
